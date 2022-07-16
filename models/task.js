780ebbef2bbb15e895b3d1e9465d1fbc1878ef99
migmanu <jmanuelmigoya@gmail.com>
2022-07-16 11:34:26 +0200

    create board schema

--- models/task.js
+++ models/task.js
@@ -4,19 +4,19 @@ const url = process.env.MONGODB_URI
 
 console.log('connecting to: ', url);
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true})
-    .then(result => {
-        console.log('connected to MongoDB');
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB', error.message);
-    })
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(result => {
+    console.log('connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB', error.message);
+  })
 
 const taskSchema = new mongoose.Schema({
-    content: String,
-    date: Date,
-    column: Number,
-    position: Number
+  content: String,
+  date: Date,
+  column: Number,
+  position: Number
 })
 
 taskSchema.set('toJSON', {
@@ -25,6 +25,10 @@ taskSchema.set('toJSON', {
     delete returnedObject._id
     delete returnedObject.__v
   }
+})
+
+const boardSchema = new mongoose.Schema({
+  board: [[taskSchema]]
 })
 
 module.exports = mongoose.model('Task', taskSchema)
